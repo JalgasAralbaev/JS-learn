6cb4d5262a260bb833546c44ce31507a3287d92d
NEURO <65507582+JalgasAralbaev@users.noreply.github.com>
2022-02-16 17:03:13 +0500

    Delete 3. String directory

--- 3. String/13.js
+++
@@ -1,5 +0,0 @@
-//Дан массив ['я', 'учу', 'javascript', '!']. С помощью метода join преобразуйте массив в строку 'я+учу+javascript+!'.
-
-let arr = ['я', 'учу', 'javascript', '!'];
-arr = arr.join('+');
-console.log(arr);
--- 3. String/14.js
+++
@@ -1,4 +0,0 @@
-//Дана строка 'я учу javascript!'. С помощью метода split запишите каждое слово этой строки в отдельный элемент массива.
-let str = 'я учу javascript!';
-str = str.split(' ');
-console.log(str);
