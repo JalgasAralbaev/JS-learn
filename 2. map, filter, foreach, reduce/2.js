285d68f0c9406b856dffa834969094c9ae346b81
NEURO <65507582+JalgasAralbaev@users.noreply.github.com>
2022-02-16 17:02:54 +0500

    Delete 2. map, filter, foreach, reduce directory

--- 2. map, filter, foreach, reduce/2.js
+++
@@ -1,6 +0,0 @@
-//Дан массив с числами. Найдите сумму этих чисел.
-
-let arr = [10, 20, 30, 40, 50, 60];
-let summ = arr.reduce((count, element) => count + element, 0);
-
-console.log(summ);
--- 2. map, filter, foreach, reduce/5.js
+++
@@ -1,8 +0,0 @@
-//Дан массив с числами. Проверьте то, что в нем есть отрицательные элементы
-const arr = [-10, 4, 5, -22, -6, 1];
-
-let newArr = arr.filter(function (element) {
-      return element < 0 ? true : ;
-   } )
-
-console.log(newArr);
